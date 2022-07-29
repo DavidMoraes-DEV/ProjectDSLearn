20bd5ffa9c7e1a2009f0959052221d28a104cc0a
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-07-29 17:28:44 -0300

    Implementando a infraestrutura de validacao

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dslearnbds.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dslearnbds.services.exceptions.DataBaseException;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	//Intercepta um tipo de exceção definida entre () e trata no método abaixo
+	@ExceptionHandler(ResourceNotFoundException.class) 
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		
+		//HttpStatus.NOT_FOUND é erro 404 e .value() pega apenas o numero inteiro
+		HttpStatus status = HttpStatus.NOT_FOUND; 
+		StandardError err = new StandardError();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value()); 
+		err.setError("Resource not found ");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	//Intercepta um tipo de exceção definida entre () e trata no método abaixo
+	@ExceptionHandler(DataBaseException.class) 
+	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class) 
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY; //Cod 422 - Alguma entidade não foi possível de ser processada
+		ValidationError err = new ValidationError();
+		
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Validation Exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		
+		for( FieldError f : e.getBindingResult().getFieldErrors()) { //Pega os erros específicos continos na validação
+			err.addError(f.getField(), f.getDefaultMessage()); //.getField() pega o valor do campo do erro, .getDefaultMessage() pega a messagem do erro do campo
+		}
+		
+		return ResponseEntity.status(status).body(err);
+	}
+}
